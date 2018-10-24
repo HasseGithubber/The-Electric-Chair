fba908bc42ed41d91366367b2448fc48689108e8
Linda Lindgren <Linda.Lindgr3n@hotmail.com>
2018-10-24 14:15:29 +0200

    inventoryyyyy

--- The Electric Chair/Player.cpp
+++ The Electric Chair/Player.cpp
@@ -1,6 +1,23 @@
 #include "pch.h"
 #include "Player.h"
+#include <iomanip>
 
+
+void Player::inventory() {
+
+	cout << "--------------------------------" << "\n Inventory:\n" << "--------------------------------" << endl;
+	cout << endl;
+	for (unsigned int i = 0; i < items.size(); i++)
+	{
+
+		cout << " " << items[i] << endl;
+
+	}
+	cout << endl;
+	cout << "--------------------------------" << endl << endl;
+
+
+}
 
 
 Player::Player()
--- The Electric Chair/Player.h
+++ The Electric Chair/Player.h
@@ -1,12 +1,21 @@
 #pragma once
+#include <vector>
 class Player
 {
 public:
-	// Player variabler
-	string name;
-	string item1;
+
+	vector<string> items;
+
+	// Till senare
+	/*
+	items.push_back("Dirty Uniform");
+	items.push_back("broken hårnål");
+	items.push_back("broken nagelfil");
+	items.push_back("broken Harrypotter Bok");
+	*/
 
 public:
+	void inventory();
 
 public:
 	Player();

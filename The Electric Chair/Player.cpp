adcac2ea47248a3225bccec02c896488262c6c42
Linda Lindgren <Linda.Lindgr3n@hotmail.com>
2018-10-30 15:51:05 +0100

    hh

--- The Electric Chair/Player.cpp
+++ The Electric Chair/Player.cpp
@@ -13,9 +13,9 @@ void Player::inventory() {
 	cout << "    Inventory:" << endl;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6); // gul färg
 	cout << "  ---------------------------------" << endl;
-	//cout << endl;
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // vanlig textfärg
-	for (unsigned int i = 0; i < items.size(); i++)
+	// Skriver ut det som finns i vektorn "items", loopen gör att allt i vektorn skrivs ut.
+	for (unsigned int i = 0; i < items.size(); i++) 
 	{
 
 		cout << "    " << items[i] << endl;
@@ -28,13 +28,16 @@ void Player::inventory() {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // Avsluta med vanlig textfärg
 }
 
+// Det som ska skrivas in i inventory från första början.
 void Player::startInventory()
 {
+	// skickar in 3 items i vektorn "items".
 	items.push_back("Hairpin");
 	items.push_back("Magic Harry Potter book");
 	items.push_back("Nailfile");
 }
 
+// Ändrar de 3 första items till trasiga i inventoryn, beroende på vilken som används. Ändras i vektorn "items".
 int Player::pushBack(int x) {
 	if (x == 1)
 	{
@@ -51,6 +54,7 @@ int Player::pushBack(int x) {
 	return 0;
 }
 
+// Ger spelaren items i inventory, beroende på vilket item spelaren tar. Läggs i vektorn "items".
 void Player::giveItem(int a) {
 	switch (a)
 	{
@@ -71,16 +75,19 @@ void Player::giveItem(int a) {
 	}
 }
 
+// Ändrar spelarens uniform till REN, om spelaren tvättar den.
 void Player::changeUniform() {
 
 	items[4] = "Clean uniform";
 }
 
+// Tar bort coins från inventoryn när dem är slut. 
 void Player::changeCoins()
 {
 	items.pop_back();
 }
 
+// Tar bort allt i inventoryn, alltså i vektorn "items" FÖRUTOM de 3 första sakerna som spelaren har från början.
 void Player::ClearExtraItems()
 {
 	int count = items.size();
@@ -94,6 +101,7 @@ void Player::ClearExtraItems()
 	}
 }
 
+// Rensar ALLT i inventoryn vid Game Over.
 void Player::clearVector()
 {
 	items.clear();
